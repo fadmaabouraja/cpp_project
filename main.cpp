1db4ca0e0d7765d5ab62fbb5a5d99c868e840f52
yassinehado <149099873+yassinehado@users.noreply.github.com>
2023-10-27 10:40:56 -0700

    Update main.cpp
    added the third line from yassine bahadou

--- main.cpp
+++ main.cpp
@@ -4,4 +4,6 @@ int main()
 {
   std::cout << "this is the first line of code in c++ ";
   std::cout << "this is the second line of code in c++ ";
+  // added the third line from yassin bahadou
+  std::cout << "this is the third line of code in c++ ";
 }
